c085fe70ac1e01720ea280abdeb9e5251706b40d
Hariom <hirox.rajput@gmail.com>
2025-09-04 16:02:07 +0530

    Fix: corrected AuthContext import in ShopContext.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,18 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.jsx'
+import AuthContext from './context/authContext.jsx'
 import UserContext from './context/UserContext.jsx'
 import ShopContext from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AuthProvider>
-      <UserContext>
-        <ShopContext>
-          <App />
-        </ShopContext>
-      </UserContext>
-    </AuthProvider>
-  </BrowserRouter>
+  <AuthContext>
+    <UserContext>
+      <ShopContext>
+    <App />
+    </ShopContext>
+    </UserContext>
+    </AuthContext>
+</BrowserRouter>
+  
 )
